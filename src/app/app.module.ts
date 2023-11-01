999ebe1062bf8c5285416e08c9bb4ed38ff0f757
www-net <78052177+www-net@users.noreply.github.com>
2023-11-01 15:45:16 +0300

    refactor: moved filter, login, search-input components to the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
 import { HeaderComponent } from './header/header.component';
-import { FilterComponent } from './filter/filter.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterComponent } from './header/filter/filter.component';
 import { MaterialDesignModule } from './material-design/material-design.module';
-import { SearchInputComponent } from './search-input/search-input.component';
-import { LoginComponent } from './login/login.component';
+import { SearchInputComponent } from './header/search-input/search-input.component';
+import { LoginComponent } from './header/login/login.component';
 
 
 @NgModule({
